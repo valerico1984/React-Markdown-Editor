caaf33548dd3285a7e89544be106f18da6c21b78
Valeria Rico <valeroxanarico@gmail.com>
2022-02-16 15:59:49 -0300

    Subir proyecto

--- src/App.js
+++ src/App.js
@@ -1,25 +1,95 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {Component} from 'react';
+import {Row} from 'react-bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {marked} from 'marked'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const markdownInitial= ("# (GitHub-Flavored) Markdown Editor\n\nHi! I\'m Valeria and I\'m testing the editor. Change the text to see what happens.\n\n## Look, a list!\n\n* Fruit\n* Bread\n* Milk\n\n## And here\'s some code!\n\n```javascript\n\n$(document).ready(function(){\n\t$(\'div\').html(\'I am a div.\');\n});\n\n```\n\n## Look, an Image!\n\n![dog](https://media.istockphoto.com/photos/active-dog-running-across-an-open-field-picture-id1283131612?k=20&m=1283131612&s=612x612&w=0&h=BucGnyUieY1kQbTMCVx54yOfbrQIJCoOpcW07F1dZcQ=)\n\n.")
+
+const renderer= new marked.Renderer()
+
+marked.setOptions({
+  renderer,
+  breaks: true,
+  gfm: true,
+  })
+
+class MarkdownContainer extends Component {
+    constructor(props) {
+      super(props)
+  
+      this.state = {
+        code: markdownInitial
+        
+      }
+
+      this.handleChange=this.handleChange.bind(this)
+    }
+
+    handleChange(code){
+      this.setState({code})
+    }
+
+
+  
+    render() {
+      return (
+        <div className="container">
+          <Editor code={this.state.code}
+                  onChange={this.handleChange}/>
+          <Preview html={this.state.code}/>
+
+        </div>
+      )
+    }
+  }
+  
+const Editor =(props)=>{
+  return(
+
+  <Row>
+    
+  <section id="editor_container">
+   <div className="toolbar"><i class="fa free-code-camp"></i>"Editor"</div>
+    <textarea id="editor" onChange={(e)=>{props.onChange(e.target.value)}} type="text">{props.code}</textarea>
+    </section>
+</Row>)
 }
 
-export default App;
+const Preview=(props)=>{
+  return(
+    
+    <Row>
+    <section id="preview_container">
+      <div className="toolbar"><i class="fa free-code-camp"></i>"Preview"</div>
+  
+       <div id="preview">
+  
+      <div dangerouslySetInnerHTML={{__html:marked(props.html,{sanitize: true})}} />
+
+   </div>
+    </section>
+
+</Row>
+)
+}
+
+
+
+
+
+export default function App() {
+
+
+
+  return(
+<>
+    <MarkdownContainer/>
+    
+   
+</>
+  )
+  
+}
+
