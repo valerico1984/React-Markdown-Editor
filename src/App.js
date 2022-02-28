fe14566191e5f4eac1fc7262cb819f2a8d7588f1
Valeria Rico <valeroxanarico@gmail.com>
2022-02-28 18:25:00 -0300

    adecucación test Freecodecamp

--- src/App.js
+++ src/App.js
@@ -5,9 +5,56 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import {marked} from 'marked'
 
 
-const markdownInitial= ("# (GitHub-Flavored) Markdown Editor\n\nHi! I'm Valeria and I'm testing the editor. Change the text to see what happens.\n\n## Look, a list!\n\n* Fruit\n* Bread\n* Milk\n\n## And here's some code!\n\n```javascript\n\n$(document).ready(function(){\n\t$('div').html('I am a div.');\n});\n\n```\n\n## Look, an Image!\n\n![dog](https://media.istockphoto.com/photos/active-dog-running-across-an-open-field-picture-id1283131612?k=20&m=1283131612&s=612x612&w=0&h=BucGnyUieY1kQbTMCVx54yOfbrQIJCoOpcW07F1dZcQ=)\n\n.")
+const markdownInitial= `# Welcome to my React Markdown Previewer!
+
+## This is a sub-heading...
+### And here's some other cool stuff:
+
+Heres some code, \`<div></div>\`, between 2 backticks.
+
+\`\`\`
+// this is multi-line code:
+
+function anotherExample(firstLine, lastLine) {
+  if (firstLine == '\`\`\`' && lastLine == '\`\`\`') {
+    return multiLineCode;
+  }
+}
+\`\`\`
+
+You can also make text **bold**... whoa!
+Or _italic_.
+Or... wait for it... **_both!_**
+And feel free to go crazy ~~crossing stuff out~~.
+
+There's also [links](https://www.freecodecamp.org), and
+> Block Quotes!
+
+And if you want to get really crazy, even tables:
+
+Wild Header | Crazy Header | Another Header?
+------------ | ------------- | -------------
+Your content can | be here, and it | can be here....
+And here. | Okay. | I think we get it.
+
+- And of course there are lists.
+  - Some are bulleted.
+     - With different indentation levels.
+        - That look like this.
+
+
+1. And there are numbered lists too.
+1. Use just 1s if you want!
+1. And last but not least, let's not forget embedded images:
+
+![freeCodeCamp Logo](https://cdn.freecodecamp.org/testable-projects-fcc/images/fcc_secondary.svg)
+`
 
 const renderer= new marked.Renderer()
+renderer.link = function (href, title, text) {
+  return `<a target="_blank" href="${href}">${text}</a>`;
+};
+
 
 marked.setOptions({
   renderer,
@@ -61,11 +108,10 @@ const Preview=(props)=>{
     <section id="preview_container">
       <div className="toolbar"><i className="fa free-code-camp"></i>"Preview"</div>
   
-       <div id="preview">
-  
-      <div dangerouslySetInnerHTML={{__html:marked(props.html,{sanitize: true})}} />
+        
+      <div dangerouslySetInnerHTML={{__html:marked(props.html,{renderer: renderer})}} id="preview" />
 
-   </div>
+  
     </section>
 
 </Row>
