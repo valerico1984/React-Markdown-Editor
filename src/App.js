e471eb3a595996f140f203a2f0a994a6e6a5a0a1
Valeria Rico <valeroxanarico@gmail.com>
2022-02-28 17:59:31 -0300

    correccción bugs

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import {marked} from 'marked'
 
 
-const markdownInitial= ("# (GitHub-Flavored) Markdown Editor\n\nHi! I\'m Valeria and I\'m testing the editor. Change the text to see what happens.\n\n## Look, a list!\n\n* Fruit\n* Bread\n* Milk\n\n## And here\'s some code!\n\n```javascript\n\n$(document).ready(function(){\n\t$(\'div\').html(\'I am a div.\');\n});\n\n```\n\n## Look, an Image!\n\n![dog](https://media.istockphoto.com/photos/active-dog-running-across-an-open-field-picture-id1283131612?k=20&m=1283131612&s=612x612&w=0&h=BucGnyUieY1kQbTMCVx54yOfbrQIJCoOpcW07F1dZcQ=)\n\n.")
+const markdownInitial= ("# (GitHub-Flavored) Markdown Editor\n\nHi! I'm Valeria and I'm testing the editor. Change the text to see what happens.\n\n## Look, a list!\n\n* Fruit\n* Bread\n* Milk\n\n## And here's some code!\n\n```javascript\n\n$(document).ready(function(){\n\t$('div').html('I am a div.');\n});\n\n```\n\n## Look, an Image!\n\n![dog](https://media.istockphoto.com/photos/active-dog-running-across-an-open-field-picture-id1283131612?k=20&m=1283131612&s=612x612&w=0&h=BucGnyUieY1kQbTMCVx54yOfbrQIJCoOpcW07F1dZcQ=)\n\n.")
 
 const renderer= new marked.Renderer()
 
@@ -30,10 +30,7 @@ class MarkdownContainer extends Component {
     handleChange(code){
       this.setState({code})
     }
-
-
-  
-    render() {
+   render() {
       return (
         <div className="container">
           <Editor code={this.state.code}
@@ -51,7 +48,7 @@ const Editor =(props)=>{
   <Row>
     
   <section id="editor_container">
-   <div className="toolbar"><i class="fa free-code-camp"></i>"Editor"</div>
+   <div className="toolbar"><i className="fa free-code-camp"></i>"Editor"</div>
     <textarea id="editor" onChange={(e)=>{props.onChange(e.target.value)}} type="text">{props.code}</textarea>
     </section>
 </Row>)
@@ -62,7 +59,7 @@ const Preview=(props)=>{
     
     <Row>
     <section id="preview_container">
-      <div className="toolbar"><i class="fa free-code-camp"></i>"Preview"</div>
+      <div className="toolbar"><i className="fa free-code-camp"></i>"Preview"</div>
   
        <div id="preview">
   
